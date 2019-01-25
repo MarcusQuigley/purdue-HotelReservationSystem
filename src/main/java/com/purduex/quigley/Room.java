a55cb924bb10eb5a80a802582bb929319a16b709
[MarcusQ] <marcusquigley@gmail.com>
2019-01-24 20:13:09 -0500

    Init commit

---
+++ src/main/java/com/purduex/quigley/Room.java
@@ -0,0 +1,82 @@
+package com.purduex.quigley;
+
+public class Room {
+    private int roomNumber;
+    private int daysRented;
+    private String occupantName;
+    private String roomType;
+
+    public Room(int number, String type) {
+        roomNumber = number;
+        roomType = checkRoomType(type);
+        daysRented = 0;
+        occupantName = null;
+    }
+
+    public int getRoomNumber() {
+        return roomNumber;
+    }
+
+    public int getDaysRented() {
+        return daysRented;
+    }
+
+    public String getOccupantName() {
+        return occupantName;
+    }
+
+    public String getRoomType() {
+        return roomType;
+    }
+
+    public boolean isRoomType(String type) {
+        return type.equalsIgnoreCase(roomType);
+    }
+
+    public boolean setOccupant(String guestName, int days) {
+        if (guestName.isEmpty() || days < 1) {
+            return false;
+            //should rally raise exception instead.
+        }
+        if (isOccupied()) {
+            return false;
+        }
+        occupantName = guestName;
+        daysRented = days;
+        return true;
+    }
+
+    public void advanceDay() {
+        daysRented -= 1;
+        if (daysRented == 0) {
+            occupantName = null;
+        }
+    }
+
+    public boolean isOccupied() {
+        return daysRented > 0;
+    }
+
+    private String checkRoomType(String type) {
+        if (type.isEmpty()){// || type.isBlank()) {
+            type = "double queen";
+        }
+        String result = "";
+        switch (type.toLowerCase()) {
+            case "single king":
+                result = type;
+                break;
+            case "suite":
+                result = type;
+                break;
+            default:
+                result = "double queen";
+        }
+        return result;
+    }
+
+    public String toString() {
+        //Room 305: single king – rented
+        return "Room " + roomNumber + ": " + roomType + " - " + (isOccupied() ? "rented" : "free");
+    }
+}
---
+++ src/test/java/com/purduex/quigley/RoomTest.java
@@ -0,0 +1,15 @@
+package com.purduex.quigley;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+
+public class RoomTest
+{
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
